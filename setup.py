a853fab8b87306194ebd518765024eb62d1204e3
Piper Wolters <piperw@allenai.org>
2024-02-06 09:54:50 -0800

    Fix License classifiers

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 Software License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.9',
